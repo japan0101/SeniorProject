9ea35e117fea49774d6d4623803c0915751cd33c
Napat Wetchapun <japan.pon11@gmail.com>
2025-07-12 19:43:00 +0700

    start on stats UI

---
+++ Assets/Scripts/PlayerScript/PlayerStatsUI.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class PlayerStatsUI : MonoBehaviour
+{
+    public event Action onStatsChanged;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
