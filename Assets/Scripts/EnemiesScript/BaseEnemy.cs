acc7e95930cb686fc8636626fce97589535d8812
Madino <89445570+Maldin0@users.noreply.github.com>
2025-08-13 20:32:59 +0700

    Implement Enemy Attack
    Co-Authored-By: Napat Wetchapun <62043549+japan0101@users.noreply.github.com>

--- Assets/Scripts/EnemiesScript/BaseEnemy.cs
+++ Assets/Scripts/EnemiesScript/BaseEnemy.cs
@@ -9,20 +9,20 @@ public class BaseEnemy : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        foreach (var atk in attacks)
-        {
-            atk.OnAttack();//try to attack
-        }
+        // foreach (var atk in attacks)
+        // {
+        //     atk.OnAttack();//try to attack
+        // }
     }
     private void OnCollisionEnter(Collision collision)
     {
         // Check if collision is with PlayerAttack layer
         if (collision.gameObject.layer == 9)
         {
-            foreach (var atk in attacks)
-            {
-                atk.OnAttack();//try to attack
-            }
+            // foreach (var atk in attacks)
+            // {
+            //     atk.OnAttack();//try to attack
+            // }
             MainBullet bullet = collision.gameObject.GetComponent<MainBullet>();
             if (bullet != null)
             {
