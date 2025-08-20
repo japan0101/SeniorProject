ce0adda059eaa99ea20058f1b769de3eb394bbf5
Napat Wetchapun <japan.pon11@gmail.com>
2025-08-20 15:59:13 +0700

    Base Enemy Refector

--- Assets/Scripts/EnemiesScript/Enemy.cs
+++ Assets/Scripts/EnemiesScript/Enemy.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Unity.MLAgents.Actuators;
 using UnityEngine;
 
 namespace EnemiesScript
@@ -8,30 +9,62 @@ namespace EnemiesScript
         [Header("Enemy Data")]
         public float hp;
         public float maxHp;
+        public float moveSpeed;
+        public float rotateSpeed;
         public List<EnemyAttack> attacks = new List<EnemyAttack>();
+        protected Rigidbody rb;
+        
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-        
+            rb = GetComponent<Rigidbody>();
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (!(hp <= 0)) return;
-            Destroy(this.gameObject);
+            CheckDeath();
         }
 
-        protected abstract void Attack();
+        protected abstract void MoveAgent(int actionIndex);
+        protected abstract void RotateAgent(int actionIndex);
+        protected abstract void Attack(int atkIndex);
 
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.layer == 9)
+            {
+                MainBullet bullet = other.gameObject.GetComponent<MainBullet>();
+                if (bullet != null)
+                {
+                    //spawn hit FX then register damage
+                    TakeDamage(bullet.damage);
+                }
+            }
+        }
+        private void CheckDeath()
+        {
+            if (!(hp <= 0)) { return; }
+            Destroy(this.gameObject);
+        }
         protected abstract void OnHit(GameObject other);
 
         protected void TakeDamage(float damage)
         {
-            hp -= damage;
+            //instantiate damage number with DamageTextSpawner component
+            GetComponent<DamageTextSpawner>().SpawnDamageText(new Vector3(transform.position.x, transform.position.y + 5, transform.position.z), damage);
+            hp -= damage;// reduce health
         }
 
         protected abstract void OnKilled(GameObject other);
-
+        protected void SpeedControl()
+        {
+            Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+            if (flatVel.magnitude > moveSpeed)
+            {
+                Vector3 limitedVel = flatVel.normalized * moveSpeed;
+                rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+            }
+        }
     }
 }
--- Assets/Scripts/EnemiesScript/Melee/MeleeEnemy.cs
+++ Assets/Scripts/EnemiesScript/Melee/MeleeEnemy.cs
@@ -1,22 +1,58 @@
-﻿using UnityEngine;
+﻿using Unity.MLAgents.Actuators;
+using UnityEngine;
 
 namespace EnemiesScript.Melee
 {
     public class MeleeEnemy:Enemy
     {
-        protected override void Attack()
+        protected override void Attack(int atkIndex)
         {
-            throw new System.NotImplementedException();
+            attacks[atkIndex].OnAttack(gameObject);
+        }
+
+        protected override void MoveAgent(int actionIndex)
+        {
+            switch (actionIndex)
+            {
+                case 1: // Move forward
+                    rb.AddForce(transform.forward * moveSpeed * 10f, ForceMode.Force);
+                    // transform.position += transform.forward * _moveSpeed * Time.deltaTime;
+                    break;
+                case 2: // Move Backward
+                    rb.AddForce(-transform.forward * moveSpeed * 10f, ForceMode.Force);
+                    // transform.position -= transform.forward * _moveSpeed * Time.deltaTime;
+                    break;
+                case 3: // Stride Right
+                    rb.AddForce(transform.right * moveSpeed * 10f, ForceMode.Force);
+                    break;
+                case 4: // Stride Left
+                    rb.AddForce(-transform.right * moveSpeed * 10f, ForceMode.Force);
+                    break;
+            }
+            SpeedControl();
         }
 
         protected override void OnHit(GameObject other)
         {
-            throw new System.NotImplementedException();
+            Debug.Log("Agent sense hit");
         }
 
         protected override void OnKilled(GameObject other)
         {
-            throw new System.NotImplementedException();
+            Debug.Log("Agent sense a kill");
+        }
+
+        protected override void RotateAgent(int actionIndex)
+        {
+            switch (actionIndex)
+            {
+                case 1: // Rotate left
+                    transform.Rotate(0f, -rotateSpeed * Time.deltaTime, 0f);
+                    break;
+                case 2: // Rotate Right
+                    transform.Rotate(0f, rotateSpeed * Time.deltaTime, 0f);
+                    break;
+            }
         }
     }
 }
