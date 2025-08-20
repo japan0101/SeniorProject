bff210ce290ecc8b23a7986ed65c22ab628984dd
Maldino <65070015@kmitl.ac.th>
2025-08-20 15:59:54 +0700

    Refactoring Agent

--- Assets/Scripts/EnemiesScript/Melee/MeleeEnemyAgent.cs
+++ Assets/Scripts/EnemiesScript/Melee/MeleeEnemyAgent.cs
@@ -9,6 +9,57 @@ namespace EnemiesScript.Melee
 {
     public class MeleeEnemyAgent:Agent
     {
+        [SerializeField] private Transform target;
+        [SerializeField] private Renderer groundRenderer;
+
+        private Renderer _renderer;
         
+        [HideInInspector] public int currentEpisode;
+        [HideInInspector] public float cumulativeReward;
+
+        private Color _defaultGroundColor;
+        private Coroutine _flashGroundCoroutine;
+
+        public override void Initialize()
+        {
+            _renderer = GetComponent<Renderer>();
+            currentEpisode = 0;
+            cumulativeReward = 0f;
+
+            if (groundRenderer)
+            {
+                _defaultGroundColor = groundRenderer.material.color;
+            }
+        }
+        private IEnumerator FlashGround(Color targetColor, float duration)
+        {
+            float elapsedTime = 0f;
+        
+            groundRenderer.material.color = targetColor;
+            while (elapsedTime < duration)
+            {
+                elapsedTime += Time.deltaTime;
+                groundRenderer.material.color = Color.Lerp(targetColor, _defaultGroundColor, elapsedTime / duration);
+                yield return null;
+            }
+        }
+
+        public override void OnEpisodeBegin()
+        {
+            if (groundRenderer && cumulativeReward != 0f)
+            { 
+                Color flashColor = (cumulativeReward > 0f) ? Color.green : Color.red;
+
+                if (_flashGroundCoroutine != null)
+                {
+                    StopCoroutine(_flashGroundCoroutine);                    
+                }
+
+                _flashGroundCoroutine = StartCoroutine(FlashGround(flashColor, 1.0f));
+            }
+            
+            currentEpisode++;
+            cumulativeReward = 0f;
+        }
     }
 }
