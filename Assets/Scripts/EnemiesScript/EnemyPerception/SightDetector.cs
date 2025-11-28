005613fb1fa1570adc7864d978a767178a98f7a3
Napat Wetchapun <japan.pon11@gmail.com>
2025-11-28 13:06:34 +0700

    Add trained AI models and update prefabs
    Added ONNX models for MeleeAI, PlayerAI, and RangeAI, along with their Unity meta files. Updated class diagrams for Enemy, EnemyAI, MeleeEnemy, RangeEnemy, Trainer, and WeaponSystem to reflect new member signatures and relationships. Prefabs for AutonomousePlayers, MeleeEnemy, and RangeEnemy now reference the new AI models. Added ML-Agents timer files and demo results, including training logs and configuration files for various agents.

---
+++ Assets/Scripts/EnemiesScript/EnemyPerception/SightDetector.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SightDetector : MonoBehaviour
+{
+    public bool IsTargetInRange = false;
+    public bool IsTargetVisible = false;
+    public Vector3 targetPosition;
+    public string targetTag = "Player";
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
