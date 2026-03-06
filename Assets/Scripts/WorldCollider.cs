0b64b4e3933b01028d608fd7d318353883840660
Madino <Doomfist12341@hotmail.com>
2026-03-06 14:00:41 +0700

    Add WorldCollider and BoundUnder collider
    Add a new WorldCollider MonoBehaviour (Assets/Scripts/WorldCollider.cs) that destroys any GameObject passed to OnTriggerEnter. Update BossTrainingScene to include a new GameObject named 'BoundUnder' with a BoxCollider (size 200x1x200) and Transform at (31.3, -32.9, 0). Includes the .meta for the new script.

---
+++ Assets/Scripts/WorldCollider.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class WorldCollider : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+}
