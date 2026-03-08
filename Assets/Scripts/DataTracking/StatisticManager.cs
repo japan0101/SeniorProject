097ee2f9fbf093f9fb8f5d165d73a2e6952759be
japan0101 <japan.pon11@gmail.com>
2026-03-09 00:35:28 +0700

    Update StatisticManager.cs

--- Assets/Scripts/DataTracking/StatisticManager.cs
+++ Assets/Scripts/DataTracking/StatisticManager.cs
@@ -7,15 +7,36 @@ public class StatisticManager : MonoBehaviour
     [SerializeField] public BossEnemy enemy;
     [SerializeField] public PlayerShoot playerShoot;
     [SerializeField] public PlayerHealth playerHealth;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    float matchStarts;
 
+    private void Awake()
+    {
+        matchStarts = Time.time;
+    }
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnEnemyDamageDealt(int Amount)
+    {
+
+    }
+    public void OnPlayerDamageDealt(int Amount)
+    {
+
+    }
+    public void OnEnemyAttack()
+    {
+
+    }
+    public void OnEnemyAttackHits()
+    {
+
+    }
+    public void OnGameEnd()
+    {
+        currentStats.GameTime = Time.time - matchStarts;
     }
 }
